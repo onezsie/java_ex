78cdb25d339928d40fcd2df922d27bf15d85cf00
Dmitry Ziganshin <z98dima@mail.ru>
2022-10-02 19:27:06 +0500

    v1

---
+++ src/example06_04.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class example06_04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int num = in.nextInt();
+        if (num >= 5 && num <= 10) System.out.println("Ваше число подходит");
+        else System.out.println("Ваше число не подходит");
+    }
+}
