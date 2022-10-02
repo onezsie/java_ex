259550a1ff76e3b76ef5e3db8353c5f1467ad8c8
Dmitry Ziganshin <z98dima@mail.ru>
2022-10-02 19:52:58 +0500

    v2

--- src/example06_05.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class example06_05 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int num = in.nextInt();
-        System.out.println("Во введенном вами числе " + num / 1000 + " тысяч");
-    }
-}
